f3a275c8ee9a3edddce4aee81ec8bab861c65185
azam <azamhizbul@gmail.com>
2015-04-20 12:25:08 +0700

    penambahan

---
+++ src/main/java/org/ifunpas/rkppl/testmaven/App.java
@@ -0,0 +1,13 @@
+package org.ifunpas.rkppl.testmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
